aa2b9c70d04c3451d551080628469c6208f0c0f6
MarlaSantos <72895099+marlasantos24@users.noreply.github.com>
2022-02-27 21:16:21 -0500

    basicoPage_pt2

--- lib/src/pages/basico_page.dart
+++ lib/src/pages/basico_page.dart
@@ -9,10 +9,23 @@ final estiloSubTitulo= TextStyle(fontSize: 18.0, color: Colors.grey );
     return Scaffold(
       body: Column(
         children: <Widget>[
-          Image(
+          _crearImagen(),
+          _crearTitulo(),
+          _crearAcciones(),
+          _crearTexto()
+        ],
+      )
+    );
+  }
+
+  Widget _crearImagen(){
+    return Image(
             image: NetworkImage('https://photographycourse.net/wp-content/uploads/2014/11/Landscape-Photography-steps.jpg'),
-          ),
-          Container(
+          );
+  }
+
+  _crearTitulo() {
+    return Container(
             padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
             child: Row(
               children: <Widget>[
@@ -30,9 +43,40 @@ final estiloSubTitulo= TextStyle(fontSize: 18.0, color: Colors.grey );
                 Text('41', style: TextStyle(fontSize: 20.0))
               ],
             ),
-          )
-        ],
-      )
+          );
+  }
+
+  _crearAcciones() {
+    
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _accion(Icons.call, 'CALL'),
+        _accion(Icons.near_me, 'ROUTE'),
+        _accion(Icons.share, 'SHARE')
+        
+      ],
+    );
+
+  }
+
+  _accion(IconData icon, String texto){
+    return Column(
+          children: <Widget>[
+            Icon(icon, color: Colors.blue, size: 40.0),
+            SizedBox(height: 5.0),
+            Text(texto, style: TextStyle(fontSize: 15.0, color: Colors.blue) ,)
+          ],
+        );
+  }
+
+  _crearTexto() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+      child: Text(
+        'Et ex duis aute in veniam occaecat incididunt voluptate. Occaecat esse ad ullamco nostrud sint minim aliqua labore adipisicing. Enim ut qui nisi est. Consectetur deserunt et do laborum occaecat exercitation aute nostrud et eu.',
+        textAlign: TextAlign.justify,
+      ),
     );
   }
 }
